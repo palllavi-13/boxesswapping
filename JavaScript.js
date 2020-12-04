c1324a9a377a75ae0960fd60152c41ee1871e2a8
pallaviupadhyay <pallaviu72@gmail.com>
2020-12-04 10:43:00 +0530

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+﻿function allowDropThis(i) {
+    i.preventDefault();
+}
+
+
+
+function dragThis(i) {
+    i.dataTransfer.setData("box", i.target.id);
+}
+
+
+
+function dropThis(i) {
+    i.preventDefault();
+    var data = i.dataTransfer.getData("box");
+    i.target.appendChild(document.getElementById(data));
+}
+
+
+
+
